a942ae28c82a4f013c638de6171d6611d7db29b5
Eliteon150ping <rishensurjoobhalee@gmail.com>
2025-08-07 21:22:11 +0200

    Java Notes

--- Beginner/1-HelloWorld/HelloWorld.java
+++ Beginner/1-HelloWorld/HelloWorld.java
@@ -1,20 +1,37 @@
 package helloWorld;
 
-             // Case-sensitive(Use camel-case for naming with more than 1 word.)
-public class helloWorld {
+             // Case-sensitive(Use Pascal-Case for naming with more than 1 word for a class and Camel-case for Variables/methods.)
+public class HelloWorld {
 
-    public static void main(String[] args) { // Start of every java program(AKA the Main method).
+    public static void main(String[] args) { // Start of every Java program(AKA the Main method).
 
         System.out.println("Hello World"); // Print to the console.
     }
 }
 
-/* To run in cmd:
-*  1) Open the directory where the file is saved in cmd(Copy and paste the location eg. C:\to\your\directory).
-*  2) javac helloWorld.java (Called compiling).
-*  3) Again type java helloWorld(.java) to see the output.
-*
+/*
+
+To run in cmd:
+
+1) Open the directory where the file is saved in cmd(Copy and paste the location eg. C:\to\your\directory).
+2) javac HelloWorld.java (Called compiling).      // Compiles to HelloWorld.class
+3) Again type java HelloWorld.java to see the output.  // Runs the compiled bytecode using JVM
+
+1) What is Compiling?
+Compiling Java code is essential because the machine(Your PC) cannot understand a human-readable language like Java.
+By translating human-readable code into machine-readable language, the Java compiler ensures that the code runs and produces
+the expected output(Compiler creates bytecode from human code).
+
+2) What is JVM?
+While compiling, the JVM(Java Virtual Machine) is basicaly a virtual computer runtime engine that runs compiled machine code
+(bytecode) on any operating system eg. Windows or MacOS. It acts as a universal software computer to run bytecode for any OS.
+
+3) What is Bytecode?
+Bytecode is the result of the human written code being coverted to machine code and is stored in the form of a ".class" file
+which can then can be transfered to a different OS to be ran by that OS with zero issues. It is platform independent meaning
+it will run on ANY Operating System.
+
+
 */
 
-// Compiling Java code is essential because the machine(Your PC) cannot understand a human-readable language like Java.
-// By translating human-readable code into machine-readable language, the Java compiler ensures that the code runs and produces the expected output.
+
